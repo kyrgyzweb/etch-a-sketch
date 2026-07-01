7288225d39cf55dea2c614bb638e01a0490eecb6
Abdullah Mamazhusup <16565377+kyrgyzweb@users.noreply.github.com>
2026-07-01 21:39:26 +0600

    Trial version of the project

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 // Target the Button and Container elements
 const button = document.querySelector("#createButtonElement");
 const container = document.querySelector("#container");
+const inputElement = document.querySelector("#squareNumber");
 
-// Function to built and append the square
+
+// Function that creates squares
 button.addEventListener("click", () => {
-    for(let i = 0; i < 256; i++) {
-        
+    container.innerHTML = "";
+    const gridSize = Number(inputElement.value);
+    const totalGridSize = gridSize * gridSize;
+    if(gridSize >= 16 && gridSize <= 100) {
 
-                 // 1.Create a new div element
-    const newSquare = document.createElement("div");
-
-    // 2.Assign CSS class to give it square dimensions
-    newSquare.className = "square";
-
-    // 3.Inject the newSquare in to the DOM container
-    container.appendChild(newSquare);
-        }
-
+        for(let i = 0; i < totalGridSize; i++) {
+            const newSquare = document.createElement("div");
+            newSquare.className = 'square';
+            newSquare.style.width = `${960 / gridSize}px`;
+            newSquare.style.height = `${960 / gridSize}px`;
+            newSquare.addEventListener('mousemove', () => {
+                let current = Number(newSquare.style.opacity || 0);
+                current += 0.1;
+            
+                if (current > 1) current = 1;
+            
+                newSquare.style.opacity = current;
+            })
+            container.appendChild(newSquare);
+    
+    } 
+    } else {
+        alert('Enter only numbers between 16 and 100!');
+    }
 });
-
